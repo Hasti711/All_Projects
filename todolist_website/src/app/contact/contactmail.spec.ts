9c0dbc15516da9c3d114977aeded230aea318d40
Hasti711 <81065916+Hasti711@users.noreply.github.com>
2022-03-12 18:30:00 +0530

    Add files via upload

---
+++ todolist_website/src/app/contact/contactmail.spec.ts
@@ -0,0 +1,7 @@
+import { Contactmail } from './contactmail';
+
+describe('Contactmail', () => {
+  it('should create an instance', () => {
+    expect(new Contactmail()).toBeTruthy();
+  });
+});
---
+++ todolist_website/src/app/contact/contactmail.ts
@@ -0,0 +1,5 @@
+export class Contactmail {
+    name:string;
+    email:string;
+    msg:String;
+}
